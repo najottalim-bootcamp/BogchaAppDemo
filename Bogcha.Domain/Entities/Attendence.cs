64f5c0125738bceafe6f4e110ed10a2d368857a1
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-02 19:26:41 +0500

    Modefied two classes /Feedback, Attendence/ First Modefy of this classes

---
+++ Bogcha.Domain/Entities/Attendence.cs
@@ -0,0 +1,18 @@
+﻿using Bogcha.Domain.Enums;
+
+namespace Bogcha.Domain.Entities
+{
+    public class Attendence
+    {
+
+        public int Id { get; set; }
+        public int ClassId { get; set; }
+        public int StudentId { get; set; }
+        public Attendence_Status_Enum Attendence_Status { get; set; }
+        public DateTime Attendence_Date { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+        public int Created_By { get; set; }
+        public int Updated_By { get; set; }
+    }
+}
