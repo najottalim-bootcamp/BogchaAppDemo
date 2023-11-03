8da56cf46a8a93aaf185584486280057799dfd8e
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-03 18:30:45 +0500

    AssessmentDateRecKG models created

---
+++ Bogcha.Domain/Entities/AssessmentDateRecKG.cs
@@ -0,0 +1,19 @@
+﻿namespace Bogcha.Domain.Entities
+{
+    public class AssessmentDateRecKG
+    {
+        public int Id { get; set; }
+        public string ChId { get; set; }
+        public string ClassId { get; set; }
+        public DateTime AssessmentDate { get; set; }
+        public int Know_100 { get; set; }
+        public int Math_100 { get; set; }
+        public int Read_100 { get; set; }
+        public int Spell_100 { get; set; }
+        public int Camera_Reading_100 { get; set; }
+        public int Camere_Spelling_100 { get; set; }
+        public int Sentence_reading_100 { get; set; }
+        public int Pattern_assessment_100 { get; set; }
+        public int Name_writing_100 { get; set; }
+    }
+}
