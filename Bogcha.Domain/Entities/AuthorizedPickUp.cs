876cb1bc81f3bbd7fd51e67ef9d6599523205cb9
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-03 18:33:57 +0500

    Added Model/AuthorizedPickUp wtih this columns/ public int Id { get; set; } public string ChId { get; set; } public string? Chickenpox { get; set; } public string? Diphtheria_Tetanus_WhoopingCough { get; set; } public string? Haemophilus_influenza_typeB { get; set; } public string? Hepatsis_A { get; set; } public string? Hepatsis_B { get; set; } public string? Influenza { get; set; } public string? Measles { get; set; } public string? Meningococcal { get; set; } public string? Pneumococcal { get; set; } public string? Polio { get; set; } public string? Rotavirus { get; set; } Edited By Ulugbek

---
+++ Bogcha.Domain/Entities/AuthorizedPickUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Bogcha.Domain.Entities
+{
+    public class AuthorizedPickUp
+    {
+        public string ChId { get; set; }
+        public string AuthFName { get; set; }
+        public string AuthLName { get; set; }
+        public string gender { get; set; }
+        public string Passport { get; set; }
+        public string strAddress { get; set; }
+        public string city { get; set; }
+        public string region { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNo { get; set; }
+    }
+}
