4b1891a67c777d0ed8ab4cc99532383e46ee31ce
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-03 19:25:00 +0500

    repo interface added

--- Bogcha.DataAccess/Repositories/WithdrawalRepositories/IWithdrawalRepository.cs
+++ Bogcha.DataAccess/Repositories/WithdrawalRepositories/IWithdrawalRepository.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bogcha.Domain.Entities;
 
 namespace Bogcha.DataAccess.Repositories.WithdrawalRepositories;
 
 public interface IWithdrawalRepository
 {
+    public ValueTask<bool> CreateAsync(Withdrawal withdrawal);
+    public ValueTask<bool> UpdateAsync(int id, Withdrawal withdrawal);
+    public ValueTask<bool> DeleteAsync(int id);
+    public ValueTask<Withdrawal> GetByIdAsync(int id);
 }
