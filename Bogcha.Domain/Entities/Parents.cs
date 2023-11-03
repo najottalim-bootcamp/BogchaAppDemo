39bdab45c2305f2cda7a06091dcd2bcf00e265c1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-03 18:10:59 +0500

    Parents Yaratdim

---
+++ Bogcha.Domain/Entities/Parents.cs
@@ -0,0 +1,23 @@
+﻿namespace Bogcha.Domain.Entities;
+
+public class Parents
+{
+    public int ChId { get; set; }
+    public string FatherFullName { get; set; }
+    public string MotherFullName { get; set; }
+    public char Passport { get; set; }
+    public string StrAddress { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public char ZipCode { get; set; }
+    public char PhoneNo1 { get; set; }
+    public char PhoneNo2 { get; set; }
+    public string Email { get; set; }
+
+}
+
+
+
+
+
+
