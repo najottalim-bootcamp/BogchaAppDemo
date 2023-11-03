0ff819e7e3a106f6ffc10c94e817bdf6e4ed9e54
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-03 19:25:09 +0500

    database added

---
+++ Bogcha.DataAccess/Repositories/Database.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace Bogcha.DataAccess.Repositories;
+
+public class Database
+{
+    protected readonly SqlConnection sqlConnection;
+
+    public Database(string connectionString)
+    {
+        this.sqlConnection = new SqlConnection(connectionString);
+    }
+}
