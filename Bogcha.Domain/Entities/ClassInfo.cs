b6d5dd575d143b2c28b54b96658f9cb63219878d
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-03 18:40:58 +0500

    Added this model/ClassInfo/with this columns/         public string ClassId { get; set; }         public string ClassName { get; set; }         public string AgeGroup { get; set; }         public string RoomNo { get; set; }         public string HeadTeacher { get; set; }         public string AssistantTeacher { get; set; }
    Edited by Ulugbek

---
+++ Bogcha.Domain/Entities/ClassInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Bogcha.Domain.Entities
+{
+    public class ClassInfo
+    {
+        public string ClassId { get; set; }
+        public string ClassName { get; set; }
+        public string AgeGroup { get; set; }
+        public string RoomNo { get; set; }
+        public string HeadTeacher { get; set; }
+        public string AssistantTeacher { get; set; }
+    }
+}
