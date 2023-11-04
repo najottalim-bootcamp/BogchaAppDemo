e851fda496a14d78faeba50aca36e79af04f3833
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-04 21:21:19 +0500

    AssessmentRecKgRepository CRUD yozildi

---
+++ Bogcha.DataAccess/Repositories/AssessmentRecKGRepositories/IAssessmentRecKGRepository.cs
@@ -0,0 +1,12 @@
+﻿using Bogcha.Domain.Entities;
+
+namespace Bogcha.DataAccess.Repositories.AssessmentRecKGRepositories;
+
+public interface IAssessmentRecKGRepository
+{
+    public ValueTask<bool> CreateAsync(AssessmentRecKG assessmentRecKG);
+    public ValueTask<bool> UpdateAsync(int id,AssessmentRecKG assessmentRecKG);
+    public ValueTask<bool> DeleteAsync(int id);
+    public ValueTask<bool> GetByIdAsync(int id);
+    public ValueTask<IEnumerable<AssessmentRecKG>> GetAllAsync();
+}
