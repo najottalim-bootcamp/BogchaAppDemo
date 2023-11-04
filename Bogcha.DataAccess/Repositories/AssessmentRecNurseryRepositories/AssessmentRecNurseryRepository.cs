bc02cf01929f0d439edb57ec6e67e41a67e89e7f
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-04 21:21:58 +0500

    AssessmentRecNursery CRUD yozildi

--- Bogcha.DataAccess/Repositories/AssessmentRecNurseryRepositories/AssessmentRecNurseryRepository.cs
+++ Bogcha.DataAccess/Repositories/AssessmentRecNurseryRepositories/AssessmentRecNurseryRepository.cs
@@ -1,12 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bogcha.Domain.Entities;
+using Dapper;
+using System.Data.SqlClient;
 
-namespace Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories
+namespace Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories;
+
+public class AssessmentRecNurseryRepository : Database, IAssessmentRecNurseryRepository
 {
-    internal class AssessmentRecNurseryRepository
+    public AssessmentRecNurseryRepository(string connectionString) : base(connectionString) { }
+
+    public async ValueTask<bool> CreateAsync(AssessmentRecNursery assessmentRecNursery)
     {
+        try
+        {
+            await sqlConnection.OpenAsync();
+            string sqlQuery = "Insert into AssessmentRecNursery values(@ChId,@ClassId," +
+                "@AssessmentDate,@Reflection_5,@Social_development_5,@Emotional_development_5,@Conflict_resolution_5)";
+
+            var command = new SqlCommand(sqlQuery, sqlConnection);
+            int result = await command.ExecuteNonQueryAsync();
+            return result > 0;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+        finally
+        {
+            await sqlConnection.CloseAsync();
+        }
+    }
+    public async ValueTask<bool> DeleteAsync(int id)
+    {
+        try
+        {
+            await sqlConnection.OpenAsync();
+            string sqlQuery = "Delete from AssessmentRecNursery where id==Id";
+            var command = new SqlCommand(sqlQuery, sqlConnection);
+            command.Parameters.AddWithValue("Id", id);
+
+            int result = await command.ExecuteNonQueryAsync();
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            await sqlConnection.CloseAsync();
+        }
+    }
+    public async ValueTask<IEnumerable<AssessmentRecNursery>> GetAllAsync()
+    {
+        try
+        {
+            await sqlConnection.OpenAsync();
+            string sqlQuery = "Select * from AssessmentRecKG;";
+            IEnumerable<AssessmentRecNursery> assessmentRecKGs = await sqlConnection.QueryAsync<AssessmentRecNursery>(sqlQuery);
+            return assessmentRecKGs;
+        }
+        catch (Exception ex)
+        {
+            return Enumerable.Empty<AssessmentRecNursery>();
+        }
+        finally
+        {
+            await sqlConnection.CloseAsync();
+        }
+    }
+
+    public ValueTask<bool> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> UpdateAsync(int id, AssessmentRecNursery assessmentRecnursery)
+    {
+        throw new NotImplementedException();
     }
 }
--- Bogcha.DataAccess/Repositories/AssessmentRecNurseryRepositories/IAssessmentRecNurseryRepository.cs
+++ Bogcha.DataAccess/Repositories/AssessmentRecNurseryRepositories/IAssessmentRecNurseryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bogcha.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories
 {
     public interface IAssessmentRecNurseryRepository
     {
-
+        public ValueTask<bool> CreateAsync(AssessmentRecNursery assessmentRecKnursery);
+        public ValueTask<bool> UpdateAsync(int id, AssessmentRecNursery assessmentRecnursery);
+        public ValueTask<bool> DeleteAsync(int id);
+        public ValueTask<bool> GetByIdAsync(int id);
+        public ValueTask<IEnumerable<AssessmentRecNursery>> GetAllAsync();
     }
 }
