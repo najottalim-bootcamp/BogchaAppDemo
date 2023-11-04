b016bcc61ccc732f8fd59939fb8479b48c205dc5
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-04 21:23:09 +0500

    AssessmentRecPreK CRUD yozildi

--- Bogcha.DataAccess/Repositories/AssessmentRecPreKRepositories/IAssessmentRecPreKRepository.cs
+++ Bogcha.DataAccess/Repositories/AssessmentRecPreKRepositories/IAssessmentRecPreKRepository.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bogcha.Domain.Entities;
 
-namespace Bogcha.DataAccess.Repositories.AssessmentRecPreKRepositories
+namespace Bogcha.DataAccess.Repositories.AssessmentRecPreKRepositories;
+
+public interface IAssessmentRecPreKRepository
 {
-    public interface IAssessmentRecPreKRepository
-    {
-    }
+    public ValueTask<bool> CreateAsync(AssessmentRecPreK assessmentRecPreK);
+    public ValueTask<bool> UpdateAsync(int id, AssessmentRecPreK assessmentRecPreK);
+    public ValueTask<bool> DeleteAsync(int id);
+    public ValueTask<bool> GetByIdAsync(int id);
+    public ValueTask<IEnumerable<AssessmentRecPreK>> GetAllAsync();
 }
