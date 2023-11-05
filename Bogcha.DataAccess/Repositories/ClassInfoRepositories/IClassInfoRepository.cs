91e79aa8b5d160e3ced74f0cf8e3d735664edb9c
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-05 16:17:33 +0500

    Edited ClassInfoRepo class and ClassInfoRepo interface Added CRUD system

--- Bogcha.DataAccess/Repositories/ClassInfoRepositories/IClassInfoRepository.cs
+++ Bogcha.DataAccess/Repositories/ClassInfoRepositories/IClassInfoRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Bogcha.DataAccess.Repositories.ClassInfoRepositories
 {
-    internal interface IClassInfoRepository
+    public interface IClassInfoRepository
     {
+        public ValueTask<bool> CreateAsync(ClassInfo classInfo);
+        public ValueTask<bool> UpdateAsync(int id, ClassInfo classInfo);
+        public ValueTask<bool> DeleteAsync(int id);
+        public ValueTask<bool> GetByIdAsync(int id);
+        public ValueTask<IEnumerable<ClassInfo>> GetAllAsync();
     }
 }
