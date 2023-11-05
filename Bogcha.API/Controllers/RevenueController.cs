047c5719825ce9ec17d82e05a6655d737a44dbc7
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-05 15:11:33 +0500

    revenue controller added

---
+++ Bogcha.API/Controllers/RevenueController.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace Bogcha.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RevenueController : ControllerBase
+    {
+        private readonly IRevenueService revenueService;
+
+        public RevenueController(IRevenueService revenueService)
+        {
+            this.revenueService = revenueService;
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllRevenuesAsync() 
+        {
+            var res =await revenueService.GetAllAsync();
+            return Ok(res);
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetRevenueByIdAsync(string ChId) 
+        {
+            var res = await revenueService.GetByIdAsync(ChId);
+            return Ok(res);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateRevenueAsync(Revenue revenue)
+        {
+            var res = await revenueService.CreateAsync(revenue);
+            return Ok(res);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateRevenueAsync(Revenue revenue)
+        {
+            var res = await revenueService.UpdateAsync(revenue);
+            return Ok(res);
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteRevenueAsync(string ChId)
+        {
+            var res = await revenueService.DeleteAsync(ChId);
+            return Ok(res);
+        }
+    }
+}
