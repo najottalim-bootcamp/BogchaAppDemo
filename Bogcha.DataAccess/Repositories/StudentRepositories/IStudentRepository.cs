9fc9a50afe65805173ded4dec9fe16f3c94f84d4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-05 18:44:09 +0500

    mofied

--- Bogcha.DataAccess/Repositories/StudentRepositories/IStudentRepository.cs
+++ Bogcha.DataAccess/Repositories/StudentRepositories/IStudentRepository.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Bogcha.DataAccess.Repositories.StudentRepositories;
 
-namespace Bogcha.DataAccess.Repositories.StudentRepositories
+public interface IStudentRepository
 {
-    internal interface IStudentRepository
-    {
-    }
 }
