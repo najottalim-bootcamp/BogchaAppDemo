0e22d76097325c40e893fee08228eba4edd50260
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-05 22:03:46 +0500

    student crude

--- Bogcha.Services/GlobalUsings.cs
+++ Bogcha.Services/GlobalUsings.cs
@@ -1,2 +1,5 @@
-﻿global using Bogcha.DataAccess.Repositories.RevenuesRepositories;
+﻿global using Bogcha.DataAccess.Repositories.StudentRepositories;
+
+global using Bogcha.DataAccess.Repositories.RevenuesRepositories;
 global using Bogcha.Domain.Entities;
+
---
+++ Bogcha.Services/Services/StudentServices/StudentService.cs
@@ -0,0 +1,35 @@
+﻿namespace Bogcha.Services.Services.StudentServices;
+
+public class StudentService : IStudentService
+{
+    private IStudentRepository _studentRepository;
+
+    public StudentService(IStudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+    public async ValueTask<bool> CreateAsync(Student entity)
+    {
+        return await _studentRepository.CreateAsync(entity);
+    }
+
+    public async ValueTask<bool> DeleteAsync(string id)
+    {
+        return await _studentRepository.DeleteAsync(id);
+    }
+
+    public async ValueTask<IEnumerable<Student>> GetAllAsync()
+    {
+        return await _studentRepository.GetAllAsync();
+    }
+
+    public async ValueTask<Student> GetByIdAsync(string id)
+    {
+        return await _studentRepository.GetByIdAsync(id);
+    }
+
+    public async ValueTask<bool> UpdateAsync(Student entity)
+    {
+        return await _studentRepository.UpdateAsync(entity);
+    }
+}
