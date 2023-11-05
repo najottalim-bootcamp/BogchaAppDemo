5366915581c98ca6c3bd43a2b41854b287c17a8e
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-05 20:42:50 +0500

    menu management service updated

--- Bogcha.Services/Services/MenuManagementServices/MenuManagementService.cs
+++ Bogcha.Services/Services/MenuManagementServices/MenuManagementService.cs
@@ -2,4 +2,34 @@
 
 public class MenuManagementService : IMenuManagementService
 {
+    private readonly IMenuManagementRepository menuManagementRepository;
+
+    public MenuManagementService(IMenuManagementRepository menuManagementRepository)
+    {
+        this.menuManagementRepository = menuManagementRepository;
+    }
+    public async ValueTask<bool> CreateAsync(MenuManagement menuManagement)
+    {
+        return await menuManagementRepository.CreateAsync(menuManagement);
+    }
+
+    public async ValueTask<bool> DeleteAsync(string ChId)
+    {
+        return await menuManagementRepository.DeleteAsync(ChId);
+    }
+
+    public async ValueTask<IEnumerable<MenuManagement>> GetAllAsync()
+    {
+        return await menuManagementRepository.GetAllAsync();
+    }
+
+    public async ValueTask<MenuManagement> GetByIdAsync(string ChId)
+    {
+        return await menuManagementRepository.GetByIdAsync(ChId);
+    }
+
+    public async ValueTask<bool> UpdateAsync(MenuManagement menuManagement)
+    {
+        return await menuManagementRepository.UpdateAsync(menuManagement);
+    }
 }
