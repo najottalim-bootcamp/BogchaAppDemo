e8e0a40d46151b33287daa4dc7b3b5264c794659
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-05 15:12:52 +0500

    global usings added

---
+++ Bogcha.API/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Bogcha.DataAccess.Repositories.RevenuesRepositories;
+global using Bogcha.DataAccess.Repositories.WithdrawalRepositories;
+global using Bogcha.Domain.Entities;
+global using Microsoft.AspNetCore.Mvc;
