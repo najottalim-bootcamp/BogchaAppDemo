e7558cf5bc3dff9c986405045293dfbd8342b6be
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-05 16:10:53 +0500

    Edited BlackListRepo class and IBlackListRepo interface Added CRUD system

--- Bogcha.DataAccess/Repositories/BlackListRepositories/IBlackListRepository.cs
+++ Bogcha.DataAccess/Repositories/BlackListRepositories/IBlackListRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Bogcha.DataAccess.Repositories.BlackListRepositories
 {
-    internal interface IBlackListRepository
+    public interface IBlackListRepository
     {
+        public ValueTask<bool> CreateAsync(BlackList blackList);
+        public ValueTask<bool> UpdateAsync(int id, BlackList blackList);
+        public ValueTask<bool> DeleteAsync(int id);
+        public ValueTask<bool> GetByIdAsync(int id);
+        public ValueTask<IEnumerable<BlackList>> GetAllAsync();
     }
 }
