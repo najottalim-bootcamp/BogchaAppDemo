859ae4da19738614f81e160c9741b8122a0bc570
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2023-11-05 15:25:36 +0500

    Repositorylar qo'shdim

---
+++ Bogcha.DataAccess/Repositories/Accident_RecordsRepositories/IAccident_RecordsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Bogcha.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogcha.DataAccess.Repositories.Accident_RecordsRepositories
+{
+    public interface IAccident_RecordsRepository
+    {
+        public ValueTask<bool> CreateAsync(Accident_Records)
+    }
+}
