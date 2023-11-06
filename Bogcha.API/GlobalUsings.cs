7998818fbc4e0bcb5a1737c3bbffcd3e144f5419
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-06 12:40:06 +0500

    Global using ga ma'lumot qo'shildi

--- Bogcha.API/GlobalUsings.cs
+++ Bogcha.API/GlobalUsings.cs
@@ -2,3 +2,6 @@
 global using Bogcha.DataAccess.Repositories.WithdrawalRepositories;
 global using Bogcha.Domain.Entities;
 global using Microsoft.AspNetCore.Mvc;
+global using Bogcha.DataAccess.Repositories.AssessmentRecKGRepositories;
+global using Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories;
+global using Bogcha.DataAccess.Repositories.AssessmentRecPreKRepositories;
