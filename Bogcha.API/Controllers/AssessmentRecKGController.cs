5e7973c72a8b28e4a4378313163f0d342923483f
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-06 12:30:23 +0500

    Kontrollerlar yaratildi

---
+++ Bogcha.API/Controllers/AssessmentRecKGController.cs
@@ -0,0 +1,48 @@
+﻿using Bogcha.DataAccess.Repositories.AssessmentRecKGRepositories;
+using Bogcha.Domain.Entities;
+using Bogcha.Services.Services.RevenueServices;
+
+namespace Bogcha.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AssessmentRecKGController : ControllerBase
+{
+    private readonly IAssessmentRecKGRepository repository;
+
+    public AssessmentRecKGController(IAssessmentRecKGRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllAssessmentRecKGAsync()
+    {
+        var res = await repository.GetAllAsync();
+        return Ok(res);
+    }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAssessmentRecKGByIdAsync(int id)
+    {
+        var res=await repository.GetByIdAsync(id);
+        return Ok(res);
+    }
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateAssessmentRecKGAsync(AssessmentRecKG assessmentRecKG)
+    {
+        var res = await repository.CreateAsync(assessmentRecKG);
+        return Ok(res);
+    }
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateAssessmentRecKGAsync( int id,AssessmentRecKG assessmentRecKG)
+    {
+        var res = await repository.UpdateAsync(id,assessmentRecKG);
+        return Ok(res);
+    }
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteAssessmentRecKGAsync(int  id)
+    {
+        var res = await repository.DeleteAsync(id);
+        return Ok(res);
+    }
+}
---
+++ Bogcha.API/Controllers/AssessmentRecPreKController.cs
@@ -0,0 +1,44 @@
+﻿using Bogcha.DataAccess.Repositories.AssessmentRecPreKRepositories;
+
+namespace Bogcha.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AssessmentRecPreKController : ControllerBase
+{
+    private readonly IAssessmentRecPreKRepository assessmentRecPreKRepository;
+    public AssessmentRecPreKController(IAssessmentRecPreKRepository assessmentRecPreKRepository)
+    {
+        this.assessmentRecPreKRepository = assessmentRecPreKRepository;        
+    }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllAssessmentRecPreKAsync()
+    {
+        var res = await assessmentRecPreKRepository.GetAllAsync();
+        return Ok(res);
+    }
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAssessmentRecPreKByIdAsync(int id)
+    {
+        var res = await assessmentRecPreKRepository.GetByIdAsync(id);
+        return Ok(res);
+    }
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateAssessmentRecPreKAsync(AssessmentRecPreK assessmentRecPreK)
+    {
+        var res = await assessmentRecPreKRepository.CreateAsync(assessmentRecPreK);
+        return Ok(res);
+    }
+    [HttpPut]
+    public async ValueTask<IActionResult> UpdateAssessmentRecPreKAsync(int id, AssessmentRecPreK assessmentRecPreK)
+    {
+        var res = await assessmentRecPreKRepository.UpdateAsync(id, assessmentRecPreK);
+        return Ok(res);
+    }
+    [HttpDelete]
+    public async ValueTask<IActionResult> DeleteAssessmentRecPreKAsync(int id)
+    {
+        var res = await assessmentRecPreKRepository.DeleteAsync(id);
+        return Ok(res);
+    }
+}
