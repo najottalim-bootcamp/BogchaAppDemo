b2b804c65c9d881ff9a86681e4735cd7423c2ec5
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-06 11:49:41 +0500

    service changed

--- Bogcha.API/Program.cs
+++ Bogcha.API/Program.cs
@@ -26,7 +26,6 @@ builder.Services.AddScoped<IRevenueService, RevenueService>();
 builder.Services.AddScoped<IWithdrawalService, WithdrawalService>();
 builder.Services.AddScoped<IMealPlanService, MealPlanService>();
 builder.Services.AddScoped<IMenuManagementService, MenuManagementService>();
-builder.Services.AddScoped<IMenuManagementRepository, MenuManagementService>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 var app = builder.Build();
 
