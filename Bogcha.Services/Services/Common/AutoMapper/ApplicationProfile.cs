cf3add93181b3aafc49a7a358e50f7d8286bd87d
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-06 20:32:12 +0500

    auto mapper with methods for revenue model

---
+++ Bogcha.Services/Services/Common/AutoMapper/ApplicationProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Bogcha.Services.DTOs.RevenueDtos;
+
+namespace Bogcha.Services.Services.Common.AutoMapper;
+public class ApplicationProfile : Profile
+{
+    public ApplicationProfile()
+    {
+        CreateMap<CreateRevenueDto, Revenue>();
+        CreateMap<UpdateRevenueDto, Revenue>();
+    }
+}
