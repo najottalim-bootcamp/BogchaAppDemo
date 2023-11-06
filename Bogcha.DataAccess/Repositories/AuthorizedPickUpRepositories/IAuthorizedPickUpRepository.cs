9856ff19b5c1dd4a1c61c0a3858ffe709528d4f2
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-06 18:35:04 +0500

    Edited AuthorPickUp interface

--- Bogcha.DataAccess/Repositories/AuthorizedPickUpRepositories/IAuthorizedPickUpRepository.cs
+++ Bogcha.DataAccess/Repositories/AuthorizedPickUpRepositories/IAuthorizedPickUpRepository.cs
@@ -3,7 +3,7 @@
     public interface IAuthorizedPickUpRepository
     {
         public ValueTask<bool> CreateAsync(AuthorizedPickUp authorizedPickUp);
-        public ValueTask<bool> UpdateAsync(string ChId, AuthorizedPickUp authorizedPickUp);
+        public ValueTask<bool> UpdateAsync(AuthorizedPickUp authorizedPickUp);
         public ValueTask<bool> DeleteAsync(string ChId);
         public ValueTask<AuthorizedPickUp> GetByIdAsync(string ChId);
         public ValueTask<IEnumerable<AuthorizedPickUp>> GetAllAsync();
