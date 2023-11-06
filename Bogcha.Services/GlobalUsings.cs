0a736c76471c55c60b63b302232512a039b5da05
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-06 11:53:52 +0500

    minor changes with global using

--- Bogcha.Services/GlobalUsings.cs
+++ Bogcha.Services/GlobalUsings.cs
@@ -2,6 +2,7 @@
 global using Bogcha.DataAccess.Repositories.MealPlanRepositories;
 global using Bogcha.DataAccess.Repositories.WithdrawalRepositories;
 global using Bogcha.DataAccess.Repositories.MenuManagementRepositories;
+global using System.ComponentModel.DataAnnotations;
 
 
 global using Bogcha.Domain.Entities;
