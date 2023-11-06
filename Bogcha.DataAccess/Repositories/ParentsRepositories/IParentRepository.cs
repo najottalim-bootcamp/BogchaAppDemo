6e79d5517c99038e0f082c027a83324278e24066
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-06 12:25:25 +0500

    Parent Service

--- Bogcha.DataAccess/Repositories/ParentsRepositories/IParentRepository.cs
+++ Bogcha.DataAccess/Repositories/ParentsRepositories/IParentRepository.cs
@@ -2,4 +2,12 @@
 
 public interface IParentRepository
 {
+    public ValueTask<bool> CreateAsync(Parents entity);
+
+    public ValueTask<bool> UpdateAsync(Parents entity);
+
+    public ValueTask<bool> DeleteAsync(string id);
+
+    public ValueTask<Parents> GetByIdAsync(string id);
+    public ValueTask<IEnumerable<Parents>> GetAllAsync();
 }
