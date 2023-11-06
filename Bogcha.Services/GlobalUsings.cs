2d8ffc5aa129ef5e09e4d50c1886ee87478e49f5
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-06 12:40:44 +0500

    Global using yangilandi

--- Bogcha.Services/GlobalUsings.cs
+++ Bogcha.Services/GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿global using Bogcha.DataAccess.Repositories.RevenuesRepositories;
 global using Bogcha.Domain.Entities;
 global using Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories;
+global using Bogcha.DataAccess.Repositories.AssessmentRecKGRepositories;
