91cd2dcafc2d156f3320332adea21e71d3cccbae
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-06 12:33:09 +0500

    AssessmentRecPreK repository qo'shildi

--- Bogcha.DataAccess/Repositories/AssessmentRecPreKRepositories/IAssessmentRecPreKRepository.cs
+++ Bogcha.DataAccess/Repositories/AssessmentRecPreKRepositories/IAssessmentRecPreKRepository.cs
@@ -7,6 +7,6 @@ public interface IAssessmentRecPreKRepository
     public ValueTask<bool> CreateAsync(AssessmentRecPreK assessmentRecPreK);
     public ValueTask<bool> UpdateAsync(int id, AssessmentRecPreK assessmentRecPreK);
     public ValueTask<bool> DeleteAsync(int id);
-    public ValueTask<bool> GetByIdAsync(int id);
+    public ValueTask<AssessmentRecPreK> GetByIdAsync(int id);
     public ValueTask<IEnumerable<AssessmentRecPreK>> GetAllAsync();
 }
