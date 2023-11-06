cb69fe18085045ff240948010b5d0eac77dceb3d
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-06 12:30:51 +0500

    Program da tekshirilidi

--- Bogcha.API/Program.cs
+++ Bogcha.API/Program.cs
@@ -1,9 +1,9 @@
-using Bogcha.DataAccess.Repositories.WithdrawalRepositories;
-using Bogcha.Domain.Entities;
-
-var builder = WebApplication.CreateBuilder(args);
-var repo = new WithdrawalRepository(builder.Configuration.GetConnectionString("DefaultConnection"));
-
+using Bogcha.DataAccess.Repositories.AssessmentRecKGRepositories;
+using Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories;
+using Bogcha.DataAccess.Repositories.AssessmentRecPreKRepositories;
+using Bogcha.Services.Services.AssessmentRecKGServices;
+using Bogcha.Services.Services.AssessmentRecNurseryServices;
+using Bogcha.Services.Services.AssessmentRecPreKServices;
 using Bogcha.Services.Services.RevenueServices;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,9 +21,16 @@ string connectionString = builder.Configuration.GetConnectionString("DefaultConn
 builder.Services.AddScoped<IRevenueRepository>(x => new RevenueRepository(connectionString));
 builder.Services.AddScoped<IWithdrawalRepository>(x => new WithdrawalRepository(connectionString));
 
+builder.Services.AddScoped<IAssessmentRecKGRepository>(x => new AssessmentRecKGRepository(connectionString));
+builder.Services.AddScoped<IAssessmentRecNurseryRepository>(x => new AssessmentRecNurseryRepository(connectionString));
+builder.Services.AddScoped<IAssessmentRecPreKRepository>(x => new AssessmentRecPreKRepository(connectionString));
 
 //adding services
 builder.Services.AddScoped<IRevenueService, RevenueService>();
+builder.Services.AddScoped<IAssessmentRecKGService, AssessmentRecKGService>();
+builder.Services.AddScoped<IAssessmentRecNurseryService, AssessmentRecNurseryService>();
+builder.Services.AddScoped<IAssessmentRecPreKService, AssessmentRecPreKService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
