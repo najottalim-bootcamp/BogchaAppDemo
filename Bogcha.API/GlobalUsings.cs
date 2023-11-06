24ad509aace5aea7459ac7b7f1316adb46444815
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-06 10:44:33 +0500

    global usings addd for some repositories and services

--- Bogcha.API/GlobalUsings.cs
+++ Bogcha.API/GlobalUsings.cs
@@ -2,6 +2,10 @@
 global using Bogcha.DataAccess.Repositories.WithdrawalRepositories;
 global using Bogcha.DataAccess.Repositories.MealPlanRepositories;
 global using Bogcha.DataAccess.Repositories.MenuManagementRepositories;
+global using Bogcha.DataAccess.Repositories.Accident_RecordsRepositories;
+global using Bogcha.DataAccess.Repositories.EmployeeRepositories;
+global using Bogcha.Services.Services.Accident_RecordsServices;
+global using Bogcha.Services.Services.EmployeeServices;
 global using Bogcha.Services.Services.WithdrawalServices;
 global using Bogcha.Services.Services.RevenueServices;
 global using Bogcha.Services.Services.MealPlanServices;
