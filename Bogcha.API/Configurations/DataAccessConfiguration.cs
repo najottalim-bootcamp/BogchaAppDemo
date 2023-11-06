65dba9e007947697adc3a08df549e85f33a28e40
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-06 12:09:08 +0500

    added configuration layer for dataaccess and service

---
+++ Bogcha.API/Configurations/DataAccessConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Bogcha.API.Configurations;
+
+public static class DataAccessConfiguration
+{
+    public static void ConfigureDataAccess(this WebApplicationBuilder builder)
+    {
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        //adding repositories
+        builder.Services.AddScoped<IRevenueRepository>(x => new RevenueRepository(connectionString));
+        builder.Services.AddScoped<IWithdrawalRepository>(x => new WithdrawalRepository(connectionString));
+        builder.Services.AddScoped<IAccident_RecordsRepository>(x => new Accident_RecordsRepository(connectionString));
+        builder.Services.AddScoped<IEmployeeRepository>(x => new EmployeeRepository(connectionString));
+        builder.Services.AddScoped<IMealPlanRepository>(x => new MealPlanRepository(connectionString));
+        builder.Services.AddScoped<IMenuManagementRepository>(x => new MenuManagementRepository(connectionString));
+    }
+}
