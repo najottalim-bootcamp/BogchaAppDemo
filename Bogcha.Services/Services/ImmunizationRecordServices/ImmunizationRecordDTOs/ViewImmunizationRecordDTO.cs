22f021b746e6d3e1240be26e254156eba4b6ea8c
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-07 20:25:22 +0500

    view immunization

---
+++ Bogcha.Services/Services/ImmunizationRecordServices/ImmunizationRecordDTOs/ViewImmunizationRecordDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Bogcha.Infrastructure.Services.ImmunizationRecordServices.ImmunizationRecordDTOs;
+public class ViewImmunizationRecordDTO
+{
+    public int Id { get; set; }
+    public string CHId { get; set; }
+    public string ChFName { get; set; }
+    public string ChLName { get; set; }
+
+    public string? Chickenpox { get; set; }
+    public string? Diphtheria_Tetanus_WhoopingCough { get; set; }
+    public string? Haemophilus_influenza_typeB { get; set; }
+    public string? Hepatsis_A { get; set; }
+    public string? Hepatsis_B { get; set; }
+    public string? Influenza { get; set; }
+    public string? Measles { get; set; }
+    public string? Meningococcal { get; set; }
+    public string? Pneumococcal { get; set; }
+    public string? Polio { get; set; }
+    public string? Rotavirus { get; set; }
+}
