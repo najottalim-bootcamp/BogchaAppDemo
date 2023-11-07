bd0a73f66583dfa86505bf792a6db7243c2eb4cf
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-07 15:52:41 +0500

    service

--- Bogcha.Services/Services/ParentsServices/IParentsService.cs
+++ Bogcha.Services/Services/ParentsServices/IParentsService.cs
@@ -1,5 +1,12 @@
-﻿namespace Bogcha.Infrastructure.Services.ParentsServices;
+﻿using Bogcha.Infrastructure.Services.ParentsServices.ParentsDtos;
 
-public interface IParentsService : IParentRepository
+namespace Bogcha.Infrastructure.Services.ParentsServices;
+
+public interface IParentsService 
 {
+    public ValueTask<IEnumerable<ViewParentDto>> GetAllRevenuesAsync();
+    public ValueTask<ViewParentDto> GetRevenueByIdAsync(string id);
+    public ValueTask<bool> UpdateAsync(string chId, ViewParentDto viewParentDto);
+    public ValueTask<bool> DeleteAsync(string chId);
+    public ValueTask<bool> CreateAsync(ViewParentDto viewParentDto);
 }
