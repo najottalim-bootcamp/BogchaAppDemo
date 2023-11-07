321288fcf1929abe6d2d98e583a4463f20d96f55
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-07 17:48:29 +0500

    Employee service

--- Bogcha.Services/Services/StudentServices/IStudentService.cs
+++ Bogcha.Services/Services/StudentServices/IStudentService.cs
@@ -1,5 +1,14 @@
-﻿namespace Bogcha.Infrastructure.Services.StudentServices;
+﻿
+using Bogcha.Infrastructure.Services.StudentServices.StudensDtos;
 
-public interface IStudentService : IStudentRepository
+namespace Bogcha.Infrastructure.Services.StudentServices;
+
+public interface IStudentService 
 {
+    public ValueTask<IEnumerable<ViewStudentDto>> GetAllStudentsAsync();
+    public ValueTask<ViewStudentDto> GetStudentByIdAsync(string id);
+    public ValueTask<bool> UpdateAsync(string chId, UpdateStudentsDto viewEmployeeDto);
+    public ValueTask<bool> DeleteAsync(string chId);
+    public ValueTask<bool> CreateAsync(CreateStudentsDto viewEmployeeDto);
+
 }
