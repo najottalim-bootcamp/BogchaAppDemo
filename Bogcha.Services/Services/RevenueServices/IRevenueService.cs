ea977982fa5b5297fcd356b184718b4049046ad1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 10:03:05 +0500

    revenue service modified

--- Bogcha.Services/Services/RevenueServices/IRevenueService.cs
+++ Bogcha.Services/Services/RevenueServices/IRevenueService.cs
@@ -1,5 +1,12 @@
-﻿namespace Bogcha.Services.Services.RevenueServices;
+﻿using Bogcha.Services.Services.RevenueServices.RevenueDtos;
 
-public interface IRevenueService : IRevenueRepository
+namespace Bogcha.Services.Services.RevenueServices;
+
+public interface IRevenueService
 {
+    public ValueTask<IEnumerable<ViewRevenueDto>> GetAllRevenuesAsync();
+    public ValueTask<ViewRevenueDto> GetRevenueByIdAsync(string id);
+    public ValueTask<bool> UpdateAsync(string chId, UpdateRevenueDto updateRevenueDto);
+    public ValueTask<bool> DeleteAsync(string chId);
+    public ValueTask<bool> CreateAsync(CreateRevenueDto createRevenueDto);
 }
--- Bogcha.Services/Services/RevenueServices/RevenueService.cs
+++ Bogcha.Services/Services/RevenueServices/RevenueService.cs
@@ -1,34 +1,97 @@
-﻿namespace Bogcha.Services.Services.RevenueServices;
+﻿using AutoMapper;
+using Bogcha.Services.Services.RevenueServices.RevenueDtos;
+
+namespace Bogcha.Services.Services.RevenueServices;
 
 public class RevenueService : IRevenueService
 {
-    private readonly IRevenueRepository revenueRepository;
+    private readonly IRevenueRepository _revenueRepository;
+    private readonly IStudentRepository _studentRepository;
+    private readonly IMapper _mapper;
 
-    public RevenueService(IRevenueRepository revenueRepository)
+    public RevenueService(
+        IRevenueRepository revenueRepository,
+        IStudentRepository studentRepository,
+        IMapper mapper)
     {
-        this.revenueRepository = revenueRepository;
-    }
-
-    public ValueTask<IEnumerable<Revenue>> GetAllAsync()
-    {
-        return revenueRepository.GetAllAsync();
-    }
-
-    public ValueTask<Revenue> GetByIdAsync(string ChId)
-    {
-        return revenueRepository.GetByIdAsync(ChId);
-    }
-    public ValueTask<bool> CreateAsync(Revenue revenue)
-    {
-        return revenueRepository.CreateAsync(revenue);
+        _revenueRepository = revenueRepository;
+        _studentRepository = studentRepository;
+        _mapper = mapper;
     }
 
-    public ValueTask<bool> UpdateAsync(Revenue revenue)
+    public async ValueTask<bool> CreateAsync(CreateRevenueDto createRevenueDto)
     {
-        return revenueRepository.UpdateAsync(revenue);
+        var revenue = _mapper.Map<Revenue>(createRevenueDto);
+        bool result =await _revenueRepository.CreateAsync(revenue);
+        return result;
     }
-    public ValueTask<bool> DeleteAsync(string ChId)
+
+    public async ValueTask<bool> DeleteAsync(string chId)
     {
-        return revenueRepository.DeleteAsync(ChId);
+        bool result =await _revenueRepository.DeleteAsync(chId);
+        return result;
+    }
+
+    public async ValueTask<IEnumerable<ViewRevenueDto>> GetAllRevenuesAsync()
+    {
+        var students = await _studentRepository.GetAllAsync();
+        var revenues = await _revenueRepository.GetAllAsync();
+
+        var revenueViews = revenues.Join(students,
+            rev => rev.ChId,
+            st => st.CHId,
+            (rev, std) => new ViewRevenueDto()
+            {
+                CHId = std.CHId,
+                ChFName = std.ChFName,
+                ChLName = std.ChLName,
+                RegistrationFee = rev.RegistrationFee,
+                Term1 = rev.Term1,
+                Term2 = rev.Term2,
+                Term3 = rev.Term3,
+                Book = rev.Book,
+                InvoiceNo = rev.InvoiceNo,
+                RecieptNo = rev.RecieptNo,
+            });
+        return revenueViews;
+    }
+
+    public async ValueTask<ViewRevenueDto> GetRevenueByIdAsync(string id)
+    {
+        Student? student = await _studentRepository.GetByIdAsync(id);
+        
+        Revenue? revenue = await _revenueRepository.GetByIdAsync(id);
+        if(student is null || revenue is null)
+        {
+            return null;
+        }
+        var revenueView = new ViewRevenueDto()
+        {
+            CHId = student.CHId,
+            ChFName = student.ChFName,
+            ChLName = student.ChLName,
+            RegistrationFee = revenue.RegistrationFee,
+            Term1 = revenue.Term1,
+            Term2 = revenue.Term2,
+            Term3 = revenue.Term3,
+            Book = revenue.Book,
+            InvoiceNo = revenue.InvoiceNo,
+            RecieptNo = revenue.RecieptNo,
+        };
+        return revenueView;
+    }
+
+    public async ValueTask<bool> UpdateAsync(string chId, UpdateRevenueDto updateRevenueDto)
+    {
+        var revenue =await _revenueRepository.GetByIdAsync(chId);
+        if (revenue is null)
+        {
+            return false;
+        }
+        revenue = _mapper.Map<Revenue>(updateRevenueDto);
+        revenue.ChId = chId;
+
+        bool result = await _revenueRepository.UpdateAsync(revenue);
+        return result;
     }
 }
