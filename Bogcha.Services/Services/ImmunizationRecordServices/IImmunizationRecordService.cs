7ff324a017dc7d90cc33819cc49f62812e6c56b2
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-07 20:58:54 +0500

    IImmunizationRecordRepository

--- Bogcha.Services/Services/ImmunizationRecordServices/IImmunizationRecordService.cs
+++ Bogcha.Services/Services/ImmunizationRecordServices/IImmunizationRecordService.cs
@@ -1,4 +1,11 @@
-﻿namespace Bogcha.Infrastructure.Services.ImmunizationRecordServices;
-public interface IImmunizationRecordService : IImmunizationRecordRepository
+﻿using Bogcha.Infrastructure.Services.ImmunizationRecordServices.ImmunizationRecordDTOs;
+
+namespace Bogcha.Infrastructure.Services.ImmunizationRecordServices;
+public interface IImmunizationRecordService
 {
+    public ValueTask<bool> CreateAsync(CreateImmunizationRecordDTO immunizationRecord);
+    public ValueTask<bool> UpdateAsync(int id, UpdateImmunizationRecordDTO immunizationRecord);
+    public ValueTask<bool> DeleteAsync(int Id);
+    public ValueTask<ViewImmunizationRecordDTO> GetByIdAsync(int id);
+    public ValueTask<IEnumerable<ViewImmunizationRecordDTO>> GetAllAsync();
 }
