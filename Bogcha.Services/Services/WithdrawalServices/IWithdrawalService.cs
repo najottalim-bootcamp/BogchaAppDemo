4c683b7a1fcb7b8fbe62368cd061804e9a596757
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 11:39:19 +0500

    service modfied

--- Bogcha.Services/Services/WithdrawalServices/IWithdrawalService.cs
+++ Bogcha.Services/Services/WithdrawalServices/IWithdrawalService.cs
@@ -1,6 +1,10 @@
 ﻿namespace Bogcha.Infrastructure.Services.WithdrawalServices;
 
-public interface IWithdrawalService : IWithdrawalRepository
+public interface IWithdrawalService
 {
-
+    ValueTask<bool> CreateAsync(CreateWithdrawalDto createWithdrawal);
+    ValueTask<bool> DeleteAsync(int id);
+    ValueTask<IEnumerable<ViewWithdrawalDto>> GetAllAsync();
+    ValueTask<Withdrawal> GetByIdAsync(int id);
+    ValueTask<bool> UpdateAsync(int id, UpdateWithdrawalDto updateWithdrawal);
 }
--- Bogcha.Services/Services/WithdrawalServices/WithdrawalService.cs
+++ Bogcha.Services/Services/WithdrawalServices/WithdrawalService.cs
@@ -2,34 +2,89 @@
 
 public class WithdrawalService : IWithdrawalService
 {
-    private readonly IWithdrawalRepository withdrawalRepository;
+    private readonly IWithdrawalRepository _withdrawalRepository;
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IMapper _mapper;
 
-    public WithdrawalService(IWithdrawalRepository withdrawalRepository)
+    public WithdrawalService(
+        IWithdrawalRepository withdrawalRepository,
+        IEmployeeRepository employeeRepository,
+        IMapper mapper
+        )
     {
-        this.withdrawalRepository = withdrawalRepository;
+        _withdrawalRepository = withdrawalRepository;
+        _employeeRepository = employeeRepository;
+        _mapper = mapper;
     }
-    public async ValueTask<bool> CreateAsync(Withdrawal withdrawal)
+    public async ValueTask<bool> CreateAsync(CreateWithdrawalDto createWithdrawal)
     {
-        return await withdrawalRepository.CreateAsync(withdrawal);
+        Withdrawal withdrawal = _mapper.Map<Withdrawal>(createWithdrawal);
+
+        bool result = await _withdrawalRepository.CreateAsync(withdrawal);
+        return result;
     }
+
 
     public async ValueTask<bool> DeleteAsync(int id)
     {
-        return await withdrawalRepository.DeleteAsync(id);
+        bool result = await _withdrawalRepository.DeleteAsync(id);
+        return result;
     }
 
-    public async ValueTask<IEnumerable<Withdrawal>> GetAllAsync()
+    public async ValueTask<IEnumerable<ViewWithdrawalDto>> GetAllAsync()
     {
-        return await withdrawalRepository.GetAllAsync();
+        IEnumerable<Withdrawal> withdrawals = await _withdrawalRepository.GetAllAsync();
+        IEnumerable<Employee> employees = await _employeeRepository.GetAllAsync();
+
+        if (!(withdrawals.Any() && employees.Any()))
+        {
+            return Enumerable.Empty<ViewWithdrawalDto>();
+        }
+        IEnumerable<ViewWithdrawalDto> viewWithdrawalDtos = withdrawals.Join(
+            employees,
+            withdrawal => withdrawal.WithDrawnBy,
+            emp => emp.EmpId,
+            (withdrawal, employee) => new ViewWithdrawalDto()
+            {
+                Id = withdrawal.Id,
+                Expense = withdrawal.Expense,
+                Amount = withdrawal.Amount,
+                DatePaid = withdrawal.DatePaid,
+                EmployeeFName = employee.EmpFName,
+                EmployeeLName = employee.EmpLName,
+                Department = employee.Department
+            });
+        return viewWithdrawalDtos;
     }
 
     public async ValueTask<Withdrawal> GetByIdAsync(int id)
     {
-        return await withdrawalRepository.GetByIdAsync(id);
+        Withdrawal withdrawal = await _withdrawalRepository.GetByIdAsync(id);
+        Employee employee = await _employeeRepository.GetByIdAsync(withdrawal.WithDrawnBy);
+        if (employee is null || withdrawal is null)
+        {
+            return null;
+        }
+        var veiwWithdrawal = new ViewWithdrawalDto()
+        {
+            Id = withdrawal.Id,
+            Expense = withdrawal.Expense,
+            Amount = withdrawal.Amount,
+            DatePaid = withdrawal.DatePaid,
+            EmployeeFName = employee.EmpFName,
+            EmployeeLName = employee.EmpLName,
+            Department = employee.Department
+        };
+        return withdrawal;
+
     }
 
-    public async ValueTask<bool> UpdateAsync(Withdrawal withdrawal)
+    public async ValueTask<bool> UpdateAsync(int id, UpdateWithdrawalDto updateWithdrawal)
     {
-        return await withdrawalRepository.UpdateAsync(withdrawal);
+        Withdrawal withdrawal = _mapper.Map<Withdrawal>(updateWithdrawal);
+        withdrawal.Id = id;
+
+        bool result = await _withdrawalRepository.UpdateAsync(withdrawal);
+        return result;
     }
 }
