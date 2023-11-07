8c2b454607d0b9a068cd1bc69dd9d2b774ea3136
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-07 17:47:53 +0500

    Employee service

--- Bogcha.Services/Services/ParentsServices/ParentsDtos/ViewParentDto.cs
+++ Bogcha.Services/Services/ParentsServices/ParentsDtos/ViewParentDto.cs
@@ -10,16 +10,14 @@ public class ViewParentDto
     public string CHId { get; set; }
     public string ChFName { get; set; }
     public string ChLName { get; set; }
-    public string FatherFullName { get; set; }
+    public string? FatherFullName { get; set; }
     public string MotherFullName { get; set; }
     public string Passport { get; set; }
     public string StrAddress { get; set; }
     public string City { get; set; }
     public string Region { get; set; }
-    public string ZipCode { get; set; }
-    public string PhoneNo1 { get; set; }
-    public string PhoneNo2 { get; set; }
-    public string Email {  get; set; }
-
-
+    public string? ZipCode { get; set; }
+    public string? PhoneNo1 { get; set; }
+    public string? PhoneNo2 { get; set; }
+    public string? Email { get; set; }
 }
