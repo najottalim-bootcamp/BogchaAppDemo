01babb59e6e25f82d6fce83505e6ef4c72900a68
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-07 20:10:38 +0500

    controller

--- Bogcha.API/Controllers/StudentContollers/StudentController.cs
+++ Bogcha.API/Controllers/StudentContollers/StudentController.cs
@@ -1,4 +1,9 @@
-﻿namespace Bogcha.API.Controllers.StudentContollers;
+﻿using Bogcha.Domain.Entities;
+using Bogcha.Infrastructure.Services.EmployeeServices.EmployeeDtos;
+using Bogcha.Infrastructure.Services.ParentsServices.ParentsDtos;
+using Bogcha.Infrastructure.Services.StudentServices.StudensDtos;
+
+namespace Bogcha.API.Controllers.StudentContollers;
 
 [Route("api/[controller]/[action]")]
 [ApiController]
@@ -10,34 +15,47 @@ public class StudentController : ControllerBase
     {
         _student = student;
     }
-    [HttpGet]
     public async ValueTask<IActionResult> GetAllStudentsAsync()
     {
-        var res = await _student.GetAllAsync();
-        return Ok(res);
+        IEnumerable<Student> Studentss = await _student.GetAllStudentsAsync();
+
+        return Ok(Studentss);
     }
     [HttpGet]
-    public async ValueTask<IActionResult> GetStudentByIdAsync(string CHId)
+    public async ValueTask<IActionResult> GetStudentsByIdAsync(string ChId)
     {
-        var res = await _student.GetByIdAsync(CHId);
-        return Ok(res);
+        Student student = await _student.GetStudentByIdAsync(ChId);
+
+        if (student is null)
+            return NotFound(ChId);
+
+        return Ok(student);
     }
     [HttpPost]
-    public async ValueTask<IActionResult> CreateStudentAsync(Student str)
+    public async ValueTask<IActionResult> CreateAsync(CreateStudentsDto createStudentsDto)
     {
-        var res = await _student.CreateAsync(str);
-        return Ok(res);
+        bool result = await _student.CreateAsync(createStudentsDto);
+        if (result)
+        {
+            return Created(Request.GetDisplayUrl(), createStudentsDto);
+        }
+        return BadRequest(createStudentsDto);
     }
     [HttpPut]
-    public async ValueTask<IActionResult> UpdateStudentAsync(Student str)
+    public async ValueTask<IActionResult> UpdateAsync(string ChId, UpdateStudentsDto updateStudentsDto)
     {
-        var res = await _student.UpdateAsync(str);
-        return Ok(res);
+        bool result = await _student.UpdateAsync(ChId, updateStudentsDto);
+
+        if (result)
+            return NoContent();
+        return BadRequest(updateStudentsDto);
     }
     [HttpDelete]
-    public async ValueTask<IActionResult> DeleteStudentAsync(string ChId)
+    public async ValueTask<IActionResult> DeleteAsync(string ChId)
     {
-        var res = await _student.DeleteAsync(ChId);
-        return Ok(res);
+        bool result = await _student.DeleteAsync(ChId);
+        if (result)
+            return NoContent();
+        return BadRequest(result);
     }
 }
