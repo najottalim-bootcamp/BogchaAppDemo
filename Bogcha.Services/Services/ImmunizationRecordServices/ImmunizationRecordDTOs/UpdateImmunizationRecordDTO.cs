81bd0251796212117f328f4116b97846ac2c04ce
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-07 20:25:10 +0500

    Update immunization

---
+++ Bogcha.Services/Services/ImmunizationRecordServices/ImmunizationRecordDTOs/UpdateImmunizationRecordDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Bogcha.Infrastructure.Services.ImmunizationRecordServices.ImmunizationRecordDTOs;
+public class UpdateImmunizationRecordDTO
+{
+
+    public string? Chickenpox { get; set; }
+    public string? Diphtheria_Tetanus_WhoopingCough { get; set; }
+    public string? Haemophilus_influenza_typeB { get; set; }
+    public string? Hepatsis_A { get; set; }
+    public string? Hepatsis_B { get; set; }
+    public string? Influenza { get; set; }
+    public string? Measles { get; set; }
+    public string? Meningococcal { get; set; }
+    public string? Pneumococcal { get; set; }
+    public string? Polio { get; set; }
+    public string? Rotavirus { get; set; }
+}
