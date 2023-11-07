9c8c9c510d9f8dc67ac83f2f06e6f00fbc12a5a5
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 18:42:14 +0500

    service for menu management created

--- Bogcha.Services/Services/MenuManagementServices/IMenuManagementService.cs
+++ Bogcha.Services/Services/MenuManagementServices/IMenuManagementService.cs
@@ -1,5 +1,10 @@
 ﻿namespace Bogcha.Infrastructure.Services.MenuManagementServices;
 
-public interface IMenuManagementService : IMenuManagementRepository
+public interface IMenuManagementService
 {
+    ValueTask<bool> CreateMenuManagement(CreateMenuManagementDto createMenuManagementDto);
+    ValueTask<bool> DeleteMenuManagementAsync(string chId);
+    ValueTask<IEnumerable<ViewMenuManagementDto>> GetAllMenusAsync();
+    ValueTask<ViewMenuManagementDto> GetMenuManagementByIdAsync(string chId);
+    ValueTask<bool> UpdateMenuManagementAsync(string chId, UpdateMealPlanDto updateMealPlanDto);
 }
