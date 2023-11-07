d4abb1c477e3504a8eca5ec4bc4b516808995bf4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-07 17:46:43 +0500

    Employee service

---
+++ Bogcha.Services/Services/EmployeeServices/EmployeeDtos/UpdateEmployeeDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogcha.Infrastructure.Services.EmployeeServices.EmployeeDtos;
+public class UpdateEmployeeDto
+{
+    public string EmpFName { get; set; }
+    public string EmpLName { get; set; }
+    public string Passport { get; set; }
+    public DateTime DoB { get; set; }
+    public string Gender { get; set; }
+    public Decimal Salary { get; set; }
+    public DateTime EmployedDate { get; set; }
+    public string StrAddress { get; set; }
+    public string Apt { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string ZipCode { get; set; }
+    public string PhoneNo { get; set; }
+    public string Email { get; set; }
+    public string EmpType { get; set; }
+    public string Department { get; set; }
+}
