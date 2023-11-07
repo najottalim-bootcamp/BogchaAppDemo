0daa331ddd0dd383347484aafc51cdb5e6659e60
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-07 17:48:21 +0500

    Employee service

---
+++ Bogcha.Services/Services/StudentServices/StudensDtos/ViewStudentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogcha.Infrastructure.Services.StudentServices.StudensDtos;
+public class ViewStudentDto
+{
+
+}
