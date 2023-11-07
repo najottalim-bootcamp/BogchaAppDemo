47161c05e3a74e39a13df611ad789f0bd479196d
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-07 20:24:14 +0500

    View black list

--- Bogcha.Services/Services/BlackListServices/BlackListDTOs/ViewBlackListDTO.cs
+++ Bogcha.Services/Services/BlackListServices/BlackListDTOs/ViewBlackListDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bogcha.Infrastructure.Services.BlackListServices.BlackListDTOs;
+﻿namespace Bogcha.Infrastructure.Services.BlackListServices.BlackListDTOs;
 public class ViewBlackListDTO
 {
     public string CHId { get; set; }
