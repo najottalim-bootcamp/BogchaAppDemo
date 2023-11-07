b32c29a1395c6ab0849cb2d830dcd6e85deb7312
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 18:41:38 +0500

    menumangement dtos created

---
+++ Bogcha.Services/Services/MenuManagementServices/MenuManagementDtos/ViewMenuManagementDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Bogcha.Infrastructure.Services.MenuManagementServices.MenuManagementDtos;
+public class ViewMenuManagementDto
+{
+    public string ChId { get; set; }
+    public string StudentFName { get; set; }
+    public string StudentLName { get; set; }
+    public string Monday { get; set; }
+    public MealPlan MondayMealPlan { get; set; }
+    public string Tuesday { get; set; }
+    public MealPlan TuesdayMealPlan { get; set; }
+    public string Wednesday { get; set; }
+    public MealPlan WednesdayMealPlan { get; set; }
+    public string Thursday { get; set; }
+    public MealPlan ThursdayMealPlan { get; set; }
+    public string Friday { get; set; }
+    public MealPlan FridayMealPlan { get; set; }
+}
