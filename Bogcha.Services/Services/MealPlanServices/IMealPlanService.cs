e812c2c7c51d56e8b1b9f588627e6ae3014c4520
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 16:06:14 +0500

    mealplan service added

--- Bogcha.Services/Services/MealPlanServices/IMealPlanService.cs
+++ Bogcha.Services/Services/MealPlanServices/IMealPlanService.cs
@@ -1,6 +1,10 @@
 ﻿namespace Bogcha.Infrastructure.Services.MealPlanServices;
 
-public interface IMealPlanService : IMealPlanRepository
+public interface IMealPlanService
 {
-
+    ValueTask<bool> CreateMealPlanAsync(CreateMealPlanDto mealPlanDto);
+    ValueTask<bool> DeleteMealPlanAsync(string mealNo);
+    ValueTask<IEnumerable<MealPlan>> GetAllMealPlansAsync();
+    ValueTask<MealPlan> GetMealPlanByIdAsync(string mealNo);
+    ValueTask<bool> UpdateMealPlanAsync(string mealNo, UpdateMealPlanDto updateMealPlanDto);
 }
