d6d7b1d3623218b52c7a77882c9a6b6896c89d1c
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 11:38:55 +0500

    withdrawal service completed

---
+++ Bogcha.Services/Services/WithdrawalServices/WithdrawalDtos/ViewWithdrawalDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Bogcha.Infrastructure.Services.WithdrawalServices.WithdrawalDtos;
+public class ViewWithdrawalDto
+{
+    public int Id { get; set; }
+    public string Expense { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime DatePaid { get; set; }
+    public string WithDrawnBy { get; set; }
+    public string EmployeeFName { get; set; }
+    public string EmployeeLName { get; set; }
+    public string Department { get; set; }
+}
