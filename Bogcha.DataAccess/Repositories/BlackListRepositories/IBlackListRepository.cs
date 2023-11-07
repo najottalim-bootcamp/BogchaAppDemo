4e1cd80f09c4907e8315ba8b876b5410a86a54f2
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-11-07 12:19:56 +0500

    Edited IBlackListRepo

--- Bogcha.DataAccess/Repositories/BlackListRepositories/IBlackListRepository.cs
+++ Bogcha.DataAccess/Repositories/BlackListRepositories/IBlackListRepository.cs
@@ -3,7 +3,7 @@
     public interface IBlackListRepository
     {
         public ValueTask<bool> CreateAsync(BlackList blackList);
-        public ValueTask<bool> UpdateAsync(string ChId, BlackList blackList);
+        public ValueTask<bool> UpdateAsync(BlackList blackList);
         public ValueTask<bool> DeleteAsync(string ChId);
         public ValueTask<BlackList> GetByIdAsync(string ChId);
         public ValueTask<IEnumerable<BlackList>> GetAllAsync();
