2046cdbf872da888b5c8d4ce710f404afd6914d8
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-11-07 10:00:00 +0500

    global using modified

--- Bogcha.Services/GlobalUsings.cs
+++ Bogcha.Services/GlobalUsings.cs
@@ -2,7 +2,11 @@
 global using Bogcha.DataAccess.Repositories.ActivityManagementRepositories;
 global using Bogcha.DataAccess.Repositories.AssessmentRecKGRepositories;
 global using Bogcha.DataAccess.Repositories.AssessmentRecNurseryRepositories;
+global using Bogcha.DataAccess.Repositories.AssessmentRecPreKRepositories;
 global using Bogcha.DataAccess.Repositories.AttendanceRepositories;
+global using Bogcha.DataAccess.Repositories.AuthorizedPickUpRepositories;
+global using Bogcha.DataAccess.Repositories.BlackListRepositories;
+global using Bogcha.DataAccess.Repositories.ClassInfoRepositories;
 global using Bogcha.DataAccess.Repositories.EmployeeRepositories;
 global using Bogcha.DataAccess.Repositories.MealPlanRepositories;
 global using Bogcha.DataAccess.Repositories.MenuManagementRepositories;
@@ -11,5 +15,7 @@ global using Bogcha.DataAccess.Repositories.RegularHealthCheckRepositories;
 global using Bogcha.DataAccess.Repositories.RevenuesRepositories;
 global using Bogcha.DataAccess.Repositories.StudentRepositories;
 global using Bogcha.DataAccess.Repositories.WithdrawalRepositories;
+global using Bogcha.Services.Services.RevenueServices.RevenueDtos;
+
 global using Bogcha.Domain.Entities;
 global using System.ComponentModel.DataAnnotations;
